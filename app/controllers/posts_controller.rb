ca1bc25f13daf6683f830f84594b986b5c940632
三浦 <miura.mai910@gmail.com>
2021-05-15 23:22:29 +0900

    投稿一覧ページに、ページネーションを利用して5件ずつ投稿を表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
+
+  PER_PAGE = 5
+
   def index
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(PER_PAGE)
   end
 
   def create
